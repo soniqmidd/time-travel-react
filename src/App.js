3ba8a5f47de2723661172ad454eb7e8f479bdbf5
soniqmidd <soniqmidd@users.noreply.github.com>
2023-11-17 00:03:06 -0600

    Added forecast layout

--- src/App.js
+++ src/App.js
@@ -1,83 +1,21 @@
+import React from "react";
+import Weather from "./Weather";
 import "./style.css";
-import WeatherInfo from "./WeatherInfo";
 
 function App() {
   return (
     <div className="App">
-      <h1>Time Travel</h1>
-      <div class="container">
-        <div class="weather-app-wrapper">
-          <div class="weather-app">
-            <form id="search-form" class="mb-3">
-              <div class="row">
-                <div class="col-6">
-                  <input
-                    type="search"
-                    placeholder="Search a city .."
-                    class="form-control"
-                    id="city-input"
-                    autocomplete="off"
-                  />
-                </div>
-
-                <div class="col-3">
-                  <input
-                    type="submit"
-                    value="Submit"
-                    class="btn btn-primary w-100"
-                  />
-                </div>
-                <div class="col-3">
-                  <input
-                    type="submit"
-                    value="Current"
-                    class="btn btn-secondary w-100"
-                  />
-                </div>
-              </div>
-            </form>
-            <div class="overview">
-              <h2 id="city"></h2>
-              <ul>
-                <li>
-                  Last updated at <span id="date"></span>
-                </li>
-                <li id="description"></li>
-              </ul>
-            </div>
-
-            <div class="row">
-              <div class="col-6">
-                <div class="outdoorfix weather-temperature">
-                  <img src="" alt="outdoor" id="icon" class="float-left" />
-                  <div class="float-left">
-                    <strong id="temperature"></strong>
-                    <span class="units">&degF</span>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-6">
-                <ul>
-                  <li>
-                    Humidity: <span id="humidity"></span> %
-                  </li>
-                  <li>
-                    Wind: <span id="wind"></span> mph
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="weather-forecast" id="forecast"></div>
-          </div>
-
+      <div className="container">
+        <h1>Time Travel</h1>
+        <Weather />
+        <footer>
           <small>
             <a href="https://github.com/soniqmidd/TimeTravel" target="_blank">
               Open Source code
             </a>
             by Sonique Middleton
           </small>
-        </div>
+        </footer>
       </div>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,103 @@
+import React from "react";
+import "./style.css";
+
+export default function Weather() {
+  return (
+    <div class="Weather">
+      <div class="weather-app-wrapper">
+        <div class="weather-app">
+          <form id="search-form" class="mb-3">
+            <div class="row">
+              <div class="col-6">
+                <input
+                  type="search"
+                  placeholder="Search a city .."
+                  class="form-control"
+                  id="city-input"
+                  autocomplete="off"
+                  autoFocus="on"
+                />
+              </div>
+
+              <div class="col-3">
+                <input
+                  type="submit"
+                  value="Submit"
+                  class="btn btn-primary w-100"
+                />
+              </div>
+              <div class="col-3">
+                <input
+                  type="submit"
+                  value="Current"
+                  class="btn btn-secondary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div class="overview">
+            <h2 id="city"></h2>
+            <ul>
+              <li>
+                <span id="date">Last updated Wednesday at 08:38</span>
+              </li>
+              <li id="description"></li>
+            </ul>
+          </div>
+
+          <div class="row">
+            <div class="col-6">
+              <div class="outdoorfix weather-temperature">
+                <img src="" alt="outdoor" id="icon" class="float-left" />
+                <div class="float-left">
+                  <strong id="temperature">60</strong>
+                  <span class="units">°F</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-6">
+              <ul>
+                <li>Humidity: 91%</li>
+                <li>Wind: 21 mph</li>
+              </ul>
+            </div>
+          </div>
+          <div class="weather-forecast" id="forecast">
+            <ul>
+              <li>
+                Thu
+                <img src="https://openweathermap.org/img/wn/04d@2x.png" /> 68°F
+              </li>
+
+              <li>
+                Fri
+                <img src="https://openweathermap.org/img/wn/04d@2x.png" /> 77°F
+              </li>
+
+              <li>
+                Sat
+                <img src="https://openweathermap.org/img/wn/02d@2x.png" /> 78°F
+              </li>
+
+              <li>
+                Sun
+                <img src="https://openweathermap.org/img/wn/10d@2x.png" /> 77°F
+              </li>
+
+              <li>
+                Mon
+                <img src="https://openweathermap.org/img/wn/10d@2x.png" /> 83°F
+              </li>
+
+              <li>
+                Tue
+                <img src="https://openweathermap.org/img/wn/10d@2x.png" /> 72°F
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
